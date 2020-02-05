fe50a5f907e479a778c0e9ead5b02fc626021798
Jan Mikolasek <jan.mikolasek@olympus.cz>
2020-02-05 10:10:47 +0100

    Fonts added

---
+++ OCGportal.MarketingEvents/Layouts/OCGportal.MarketingEvents/js/entities/participant.js
@@ -0,0 +1,14 @@
+﻿define(["knockout", "kobindings", "helpers"], function (ko) {
+    participant = function () {
+        var self = this;
+
+        self.rowID = ko.observable();
+
+        self.user = ko.observableArray();
+        self.accommodation = ko.observable(true);
+        self.accommodationFrom = ko.observable();
+        self.accommodationTo = ko.observable();
+        self.booked = ko.observable();
+    }
+});
+
